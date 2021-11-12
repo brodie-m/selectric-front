3d9873c8164d330cc7517fb53417e92e4f16d929
eledoro <lena@booggee.com>
2021-11-12 19:48:55 +0000

    Added Google maps

--- selectric/src/App.js
+++ selectric/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import * as React from 'react';
 import Landing from './Pages/Landing';
+import Dashboard from './Pages/Dashboard';
 
 function App() {
   return (
   <div>
 
   <Landing/>
+  <Dashboard/>
   </div>
   );
 }
