08d747aee025741e83a57c716174bbbad75a0c88
brodie-m <brodiedmcguire@gmail.com>
2021-11-13 20:38:35 +0000

    dash layout

--- selectric/src/App.js
+++ selectric/src/App.js
@@ -2,19 +2,31 @@ import './App.css';
 import * as React from 'react';
 import Landing from './Pages/Landing';
 import Dashboard from './Pages/Dashboard';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   return (
-  <div>
+  <Router>
+    <Switch>
+    <Route path = '/' exact> 
 
 
   
   
 
   <Landing className='gradient__bg'/>
+    </Route>
+    <Route path = '/dashboard'>
   <Dashboard/>
+      </Route>
+      </Switch>
+  </Router>
 
-  </div>
+  
   );
 }
 
--- selectric/src/Pages/Dashboard/index.js
+++ selectric/src/Pages/Dashboard/index.js
@@ -1,42 +1,42 @@
 import React from "react";
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 import { Container } from "@mui/material";
 import NavBar from "../../Components/NavBar";
-
-require('dotenv').config();
+import "./dashboard.css";
+require("dotenv").config();
 
 export default function Dashboard() {
+  const containerStyle = {
+    width: "55vw",
+    height: "55vw",
+  };
 
-
-    const containerStyle = {
-        width: '400px',
-        height: '400px'
-    };
-
-    const center = {
-        lat: 51.5012,
-        lng: -0.1354
-    };
-    return (
-        <>
-            <NavBar />
-            <Container sx={{ m: 2, p: 2, display: "flex" }}>
-                <LoadScript
-                    googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}
-
-                >
-                    <GoogleMap
-                        mapContainerStyle={containerStyle}
-                        center={center}
-                        zoom={10}
-                    >
-                        { /* Child components, such as markers, info windows, etc. */}
-                        <></>
-                    </GoogleMap>
-                </LoadScript>
-
-            </Container>
-        </>
-
-    );
+  const center = {
+    lat: 51.5012,
+    lng: -0.1354,
+  };
+  return (
+    <>
+      <NavBar />
+      <Container maxWidth="lg" sx={{ display: "flex", flexDirection: "row" }}>
+        <div className="info__holder">
+          <div className="profile__holder">hello profile</div>
+          <div className="options__holder">hello options</div>
+          <div className="directions__holder">hello directions</div>
+        </div>
+        <div className="maps__holder">
+          <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}>
+            <GoogleMap
+              mapContainerStyle={containerStyle}
+              center={center}
+              zoom={10}
+            >
+              {/* Child components, such as markers, info windows, etc. */}
+              <></>
+            </GoogleMap>
+          </LoadScript>
+        </div>
+      </Container>
+    </>
+  );
 }
