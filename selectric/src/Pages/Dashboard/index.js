47cb23abaf06aca033fb6a10ba4d0303a489e5c8
dimtaiwo <dimejitaiwo@Dimejis-MacBook-Pro.local>
2021-11-15 14:55:49 +0000

    selected connections

--- selectric/src/Pages/Dashboard/index.js
+++ selectric/src/Pages/Dashboard/index.js
@@ -1,5 +1,13 @@
 import React, { useEffect, useLayoutEffect, useState } from "react";
-import { DirectionsRenderer, DirectionsService, GoogleMap, InfoWindow, LoadScript, Marker, useLoadScript } from "@react-google-maps/api";
+import {
+  DirectionsRenderer,
+  DirectionsService,
+  GoogleMap,
+  InfoWindow,
+  LoadScript,
+  Marker,
+  useLoadScript,
+} from "@react-google-maps/api";
 import { Container } from "@mui/material";
 import NavBar from "../../Components/NavBar";
 import "./dashboard.css";
@@ -8,41 +16,37 @@ import MapOptions from "../../Components/MapOptions";
 import mapStyles from "./mapStyles";
 import Directions from "../../Components/Directions";
 require("dotenv").config();
-const zoom = 10
+const zoom = 10;
 export default function Dashboard() {
+  const libraries = ["places"];
 
-  const libraries=['places']
-
-  const {isLoaded, loadError} = useLoadScript({
+  const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: "AIzaSyCMnp0NR1KzbU5BYQP_MY8CIhBa9CigoGE",
-    libraries
-  })
+    libraries,
+  });
 
-  const [selected, setSelected] = useState(false)
-  const [markers, setMarkers] = useState([])
+  const [selected, setSelected] = useState(false);
+  const [markers, setMarkers] = useState([]);
+  const [connections, setConnections] = useState([]);
 
-  
   // const handleChange = (prop) => (event) => {
   //   console.log(event, event.target.value)
   //   setValues({ ...values, [prop]: event.target.value });
   // };
 
   function directionsCallback(response) {
-    console.log(response)
+    console.log(response);
 
     if (response !== null) {
-      if (response.status === 'OK') {
-        setValues(
-          () => ({
-            response
-          })
-        )
+      if (response.status === "OK") {
+        setValues(() => ({
+          response,
+        }));
       } else {
-        console.log('response: ', response)
+        console.log("response: ", response);
       }
     }
   }
-
 
   const [endpoints, setEndpoints] = useState({
     from: "",
@@ -51,30 +55,49 @@ export default function Dashboard() {
 
   const handleChange = (a, event) => {
     //console.log(event)//, event.target.value)
-    console.log(event.target.value)
-    console.log(a)
-    console.log(endpoints)
+    console.log(event.target.value);
+    console.log(a);
+    console.log(endpoints);
     setEndpoints({ ...endpoints, [a]: event.target.value });
-    console.log(endpoints)
+    console.log(endpoints);
   };
 
-
   function handleSubmit(event) {
-    console.log(values)
-    console.log(event)
-    console.log(endpoints)
-    setValues({ ...values, origin: endpoints.from, destination: endpoints.to, travelMode: 'DRIVING' })
-    console.log(values)
+    console.log(values);
+    console.log(event);
+    console.log(endpoints);
+    setValues({
+      ...values,
+      origin: endpoints.from,
+      destination: endpoints.to,
+      travelMode: "DRIVING",
+    });
+    console.log(values);
   }
+
+  const handleSelect = (selected) => {
+    const selectedArray = [...connections];
+    selectedArray.push(selected);
+
+    console.log(selectedArray);
+    setConnections(selectedArray);
+    console.log(connections);
+  };
+
+  const waypoints = connections.map((connection) => {
+    return {
+      location: new window.google.maps.LatLng(connection.lat, connection.lng),
+    };
+  });
 
   const [values, setValues] = useState({
     //origin: 'London',
     //destination: 'Edinburgh',
-    origin: '',
-    destination: '',
-    travelMode: 'DRIVING',
+    origin: "",
+    destination: "",
+    travelMode: "DRIVING",
     response: null,
-  })
+  });
 
   const containerStyle = {
     width: "40vw",
@@ -89,154 +112,187 @@ export default function Dashboard() {
   const options = {
     styles: mapStyles,
     disableDefaultUI: true,
-    zoomControl: true
+    zoomControl: true,
   };
 
   const infoWindowOptions = {
-    styles: mapStyles
-  }
+    styles: mapStyles,
+  };
 
   useLayoutEffect(() => {
-    
-    const titles = document.querySelectorAll('.anim')
-    const observer =new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('animate')
-            }
-            else {entry.target.classList.remove('animate')}
-        })
-    })
-    titles.forEach(title=> {
-        observer.observe(title)
-    })
+    const titles = document.querySelectorAll(".anim");
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("animate");
+        } else {
+          entry.target.classList.remove("animate");
+        }
+      });
+    });
+    titles.forEach((title) => {
+      observer.observe(title);
+    });
     async function fetchChargePoints() {
-      if (!values.response) {return}
-      const polyline = values.response && values.response.routes[0].overview_polyline
-      const distance= 1
-      const maxResults = 40
+      if (!values.response) {
+        return;
+      }
+      const polyline =
+        values.response && values.response.routes[0].overview_polyline;
+      const distance = 1;
+      const maxResults = 40;
 
       //const result = await fetch(`https://api.openchargemap.io/v3/poi/?output=json&countrycode=GB&maxresults=100?key=0c36b6d2-3cf6-4f4d-9bf9-fc72140229ab`)
-      const result = await fetch(`https://api.openchargemap.io/v3/poi/?output=json&distance=${distance}&polyline=${polyline}&maxresults=${maxResults}&key=0c36b6d2-3cf6-4f4d-9bf9-fc72140229ab`)
-      const data = await result.json()
-      console.log(data)
-      data.forEach(point => console.log(point.UsageType))
-      const markers = data.map(point => {
+      const result = await fetch(
+        `https://api.openchargemap.io/v3/poi/?output=json&distance=${distance}&polyline=${polyline}&maxresults=${maxResults}&key=0c36b6d2-3cf6-4f4d-9bf9-fc72140229ab`
+      );
+      const data = await result.json();
+      console.log(data);
+      data.forEach((point) => console.log(point.UsageType));
+      const markers = data.map((point) => {
         return {
-          "name": point.AddressInfo.Title,
-          "lat": point.AddressInfo.Latitude,
-          "lng": point.AddressInfo.Longitude,
-          "powerLevel": point.Connections[0].LevelID,
-          "connections": point.Connections.map((connection) => {
-          
-
+          name: point.AddressInfo.Title,
+          lat: point.AddressInfo.Latitude,
+          lng: point.AddressInfo.Longitude,
+          powerLevel: point.Connections[0].LevelID,
+          connections: point.Connections.map((connection) => {
             return {
               connectionType: connection.ConnectionType,
               power: connection.PowerKW,
               statusType: connection.StatusType,
-              
-            }
-
+            };
           }),
-          "usageCost": point.UsageCost,
-          "usageType": point.UsageType,
-          "operational": point.Connections.filter((connection) => {
-            return connection.StatusType.IsOperational
-          }).length ? true : false
-        }
-      })
-      setMarkers(markers)
+          usageCost: point.UsageCost,
+          usageType: point.UsageType,
+          operational: point.Connections.filter((connection) => {
+            return connection.StatusType.IsOperational;
+          }).length
+            ? true
+            : false,
+        };
+      });
+      setMarkers(markers);
     }
-    fetchChargePoints()
-    return () => {
-        
-    };
-}, [values.response])
-  
-  return ( isLoaded? 
+    fetchChargePoints();
+    return () => {};
+  }, [values.response]);
+
+  return isLoaded ? (
     <>
       <NavBar />
       <div className="maps__container">
         <div className="info__holder">
-          <div className="profile__holder anim" style={{animationDelay: '-0.3s'}}>
+          <div
+            className="profile__holder anim"
+            style={{ animationDelay: "-0.3s" }}
+          >
             <Profile />
           </div>
-          <div className="options__holder anim" style={{animationDelay: '-0.2s'}}>
-            <MapOptions handleSubmit={handleSubmit} handleChange={handleChange} values={values} setValues={setValues} />
+          <div
+            className="options__holder anim"
+            style={{ animationDelay: "-0.2s" }}
+          >
+            <MapOptions
+              handleSubmit={handleSubmit}
+              handleChange={handleChange}
+              values={values}
+              setValues={setValues}
+            />
           </div>
-          <div className="directions__holder anim"  style={{animationDelay: '-0.1s'}}>
+          <div
+            className="directions__holder anim"
+            style={{ animationDelay: "-0.1s" }}
+          >
             <Directions />
           </div>
         </div>
         <div className="maps__holder">
-          
-            <GoogleMap
-              id="dashboard-map"
-              mapContainerStyle={containerStyle}
-              center={center}
-              zoom={12}
-              options={options}
-            >
-              
-              {/* Child components, such as markers, info windows, etc. */
-              markers && markers.map((marker,index) => <Marker 
-              key = {index}
-              position = {{lat: marker.lat,lng: marker.lng}}
-              icon ={{
-                url: marker.operational ? `bolt${marker.powerLevel}.svg` : `bad-bolt.svg`,
-                scaledSize: new window.google.maps.Size(30,30),
-                origin: new window.google.maps.Point(0,0),
-                anchor: new window.google.maps.Point(10,10)
-              }}
-              onClick = {() => {
-                console.log(marker)
-                setSelected(marker)
-              }}
-              />)
-              
-              } {
-                selected && 
-                <div className = 'maps__infoWindow'>
-                <InfoWindow 
-                
-                position ={{lat: selected.lat, lng: selected.lng}}
-                onCloseClick={() => setSelected(null)}
+          <GoogleMap
+            id="dashboard-map"
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={12}
+            options={options}
+          >
+            {
+              /* Child components, such as markers, info windows, etc. */
+              markers &&
+                markers.map((marker, index) => (
+                  <Marker
+                    key={index}
+                    position={{ lat: marker.lat, lng: marker.lng }}
+                    icon={{
+                      url: marker.operational
+                        ? `bolt${marker.powerLevel}.svg`
+                        : `bad-bolt.svg`,
+                      scaledSize: new window.google.maps.Size(30, 30),
+                      origin: new window.google.maps.Point(0, 0),
+                      anchor: new window.google.maps.Point(10, 10),
+                    }}
+                    onClick={() => {
+                      setSelected(marker);
+                    }}
+                  />
+                ))
+            }{" "}
+            {selected && (
+              <div className="maps__infoWindow">
+                <InfoWindow
+                  position={{ lat: selected.lat, lng: selected.lng }}
+                  onCloseClick={() => setSelected(null)}
                 >
                   <div>
-                    <h1 className = 'gradient__text'>{selected.name}</h1>
-                    <h4>Cost: {selected.usageCost ? selected.usageCost : 'not specified'}</h4>
+                    <h1 className="gradient__text">{selected.name}</h1>
+                    <h4>
+                      Cost:{" "}
+                      {selected.usageCost
+                        ? selected.usageCost
+                        : "not specified"}
+                    </h4>
                     <h4>Payment info: {selected.usageType.Title}</h4>
-                    {selected.usageType.IsMembershipRequired ? <h4>Membership required</h4> : ''}
-                    <br/>
+                    {selected.usageType.IsMembershipRequired ? (
+                      <h4>Membership required</h4>
+                    ) : (
+                      ""
+                    )}
+                    <br />
                     {selected.connections.map((connection, index) => (
-
                       <ul key={index}>
-                    <h3>Connection #{index +1}</h3>
-                    <li>Power: {connection.power} kW </li>
-                    <li>Operational: {connection.statusType.IsOperational ? 'true' : 'false'}</li>
-                    <li></li>
+                        <h3>Connection #{index + 1}</h3>
+                        <li>Power: {connection.power} kW </li>
+                        <li>
+                          Operational:{" "}
+                          {connection.statusType.IsOperational
+                            ? "true"
+                            : "false"}
+                        </li>
+                        <li></li>
                       </ul>
-                    
                     ))}
+                    <button onClick={() => handleSelect(selected)}>
+                      Select
+                    </button>
                   </div>
-                </InfoWindow></div>}
-              
-              <DirectionsService 
-                options={{
-                  destination: values.destination,
-                  origin: values.origin,
-                  travelMode: values.travelMode
-                }}
-                callback={directionsCallback}
-
-              />
-              <DirectionsRenderer 
-                options={{directions: values.response, zoom: zoom}}
-              />
-            </GoogleMap>
-          
+                </InfoWindow>
+              </div>
+            )}
+            <DirectionsService
+              options={{
+                destination: values.destination,
+                origin: values.origin,
+                travelMode: values.travelMode,
+                waypoints: waypoints,
+              }}
+              callback={directionsCallback}
+            />
+            <DirectionsRenderer
+              options={{ directions: values.response, zoom: zoom }}
+            />
+          </GoogleMap>
         </div>
       </div>
     </>
-  : 'loading');
+  ) : (
+    "loading"
+  );
 }
