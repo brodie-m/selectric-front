6cab5f8487c71ad166745e150dc9af0fbba09483
benwato <benwato12@gmail.com>
2021-11-18 11:08:23 +0000

    about text update

--- selectric/src/Components/About/index.js
+++ selectric/src/Components/About/index.js
@@ -15,11 +15,11 @@ export default function About() {
       
         <Feature
           title="Purpose"
-          text="With the 2030 diesel and petrol ban put in place, the automotive industry is moving into a new age of an electrically powered vehicles. With that in mind, we felt the market lacked an app which is not only able to provide routes based on charging points, but also have the ability to suggest things to do whilst the car is charging. Selectric is here to ease your transition into a more eco conscious, electrically powered world."
+          text="With the 2030 diesel and petrol ban being implemented, the automotive industry is moving into a new age of electrically powered vehicles. With that in mind, we felt the market lacked an app which is not only able to provide routes based on charging points, but also have the ability to suggest things to do whilst the car is charging. Selectric is here to ease your transition into a more eco conscious, electrically powered world."
         />
         <Feature
           title="How Does It Work?"
-          text="Selectric uses data from Open Charge Maps, along with Google maps, directions, and places APIs to display routes through charging points worldwide using your car specification. However, it doesn't stop there! Selectric suggests activities to do in the local area whilst your car is charging,  based on your preferences."
+          text="Selectric uses data from Open Charge Maps, along with Google maps, directions, and places APIs to display routes through charging points worldwide using your car specification. However, it doesn't stop there! Selectric suggests activities to do in the local area whilst your car is charging, based on your preferences."
         />
       </div>
     </div>
